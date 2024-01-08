ab7791d590bb4c9378913e397ea0b22cd211d06a
Ionut Panduru <panduru.ionut@hotmail.com>
2024-01-08 15:16:06 +0200

    feat: Add fetching products to store

--- src/pinia/useProductStore.ts
+++ src/pinia/useProductStore.ts
@@ -1,4 +1,18 @@
 import { defineStore } from 'pinia'
+import { fetchProductsList } from '../helpers/productApiUtils'
+import { Product } from '../types/product-type'
+import { ref } from 'vue'
 
-export const useProductStore = defineStore('products', {
+export const useProductStore = defineStore('products', () => {
+    let products = ref<Product[]>([])
+
+    const setProducts = async () => {
+        const response = (await fetchProductsList()).products
+        products.value = response
+    }
+
+    return {
+        products,
+        setProducts
+    }
 })
